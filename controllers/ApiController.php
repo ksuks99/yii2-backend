9f40d9f590c83723eef580af90489eabcd0aaed8
ksuks99 <xssysha@yandex.ru>
2022-01-26 23:08:38 +0700

    Add action api/get and api/get?id=1

--- controllers/ApiController.php
+++ controllers/ApiController.php
@@ -10,6 +10,10 @@ use yii\filters\VerbFilter;
 use app\models\LoginForm;
 use app\models\ContactForm;
 
+use app\models\Note;
+use yii\web\HttpException;
+use yii\web\NotFoundHttpException;
+
 class ApiController extends Controller // /api
 {
   // public $serializer = [
@@ -17,22 +21,57 @@ class ApiController extends Controller // /api
   //   'collectionEnvelope' => 'items',
   // ];
 
-  public function actionTest() { // api/test
-    return 'ok';
-  }
-
-  public function actionGet() { // api/get
-    return ['id1' => 100, 'id2' => 200];
-  }
-
-  public function actionOther() { // api/other
-    return $this->render('index');
-  }
-
-  // convert array to JSON
-  public function beforeAction($action)
+  public function actionTest() // api/test
   {
     \Yii::$app->response->format = Response::FORMAT_JSON;
+    // $model = new \app\models\ContactForm;
+    $response = \Yii::$app->response;
+    throw new HttpException(404, 'The requested Item could not be found.');
+    return [
+      "isSuccessful" => $response->isSuccessful, 
+      "statusCode" => $response->statusCode, 
+      "version" => $response->version
+    ];
+  }
+
+  public function actionGet() // api/get
+  {
+    // convert array to JSON
+    \Yii::$app->response->format = Response::FORMAT_JSON;
+    $request = Yii::$app->getRequest();
+    // $myData=(object)$request->bodyParams['message'];
+    $id = $request->get('id', null);
+    if ($id === null) { // 0 == null
+      // return Note::findBySql("SELECT * FROM note")->all();
+      return Note::find()->all();
+    }
+    return $this->findModel(intval($id));
+  }
+
+  public function actionInsert() // api/insert
+  {
+    \Yii::$app->response->format = Response::FORMAT_JSON;
+    // $request = Yii::$app->getRequest();
+    $request = $this->request;
+    $post = $request->post();
+
+    return $post;
+  }
+
+  public function beforeAction($action)
+  {
+    if ($action->id === 'insert') {
+      
+    }
     return parent::beforeAction($action);
+  }
+
+  protected function findModel($id)
+  {
+      if (($model = Note::findOne($id)) !== null) {
+          return $model;
+      }
+
+      throw new NotFoundHttpException(Yii::t('app', 'The requested page does not exist.'));
   }
 }
--- controllers/UserController.php
+++ controllers/UserController.php
@@ -7,9 +7,27 @@ use app\models\User;
 use yii\web\Controller;
 use yii\web\NotFoundHttpException;
 use yii\filters\VerbFilter;
+use yii\filters\AccessControl;
 
 class UserController extends Controller // user/
 {
+  public function behaviors()
+  {
+    $behaviors = parent::behaviors();
+
+    // redirect to login
+    $behaviors['access'] = [
+      'class' => AccessControl::class,
+      'rules' => [
+        [
+          'allow' => true,
+          'roles' => ['@']
+        ],
+      ],
+    ];
+    return $behaviors;
+  }
+
   public function actionExist($id) // user/view?id=100
   {
     // /web/user/exist?id=101
